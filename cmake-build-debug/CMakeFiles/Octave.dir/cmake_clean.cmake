9b85cb79047510d64d8eee5a6587a4b3d3c7001e
jimena <jimena@debian>
2017-02-15 13:43:58 -0600

    Inicio del proyecto

---
+++ cmake-build-debug/CMakeFiles/Octave.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Octave.dir/main.cpp.o"
+  "Octave.pdb"
+  "Octave"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Octave.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
